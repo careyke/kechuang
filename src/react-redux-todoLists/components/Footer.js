009f00d06cfca4bf6590a0267125e37b1e45d909
kechuang <wutkechuang@163.com>
2019-01-14 13:33:09 +0800

    fix:bug

--- src/react-redux-todoLists/components/Footer.js
+++ src/react-redux-todoLists/components/Footer.js
@@ -2,6 +2,7 @@
  * Footer
  */
 import React from 'react';
+import classnames from 'classnames';
 import styels from './Footer.less';
 
 export default class Footer extends React.Component {
@@ -18,20 +19,25 @@ export default class Footer extends React.Component {
         let leftList = todolist.filter((todo) => {
             return todo.get('completed') !== true
         })
-        return leftList.size;
+        return { leftNum: leftList.size, hideClear: todolist.size === 0 || leftList.size === todolist.size }
     }
 
     render() {
         let { activeKey, switchTab } = this.props;
+        let info = this.getLeftNum();
+        let clearClass = classnames({
+            [styels['clear']]: true,
+            [styels['hideClear']]: info.hideClear
+        })
         return (
             <div className={styels['footer']}>
-                <div className={styels['banner']}>{`${this.getLeftNum()} items left`}</div>
+                <div className={styels['banner']}>{`${info.leftNum} items left`}</div>
                 <div className={styels['tab']}>
                     <button className={activeKey === 0 ? styels['activeBtn'] : null} onClick={() => { switchTab(0) }} >{'All'}</button>
                     <button className={activeKey === 1 ? styels['activeBtn'] : null} onClick={() => { switchTab(1) }}>{'Active'}</button>
                     <button className={activeKey === 2 ? styels['activeBtn'] : null} onClick={() => { switchTab(2) }}>{'Completed'}</button>
                 </div>
-                <div className={styels['clear']} onClick={this.handleClear} >{'Clear Completed'}</div>
+                <div className={clearClass} onClick={this.handleClear} >{'Clear Completed'}</div>
             </div>
         )
     }
